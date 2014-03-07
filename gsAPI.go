636c4b28b707f54d678b0214a15155d294acbf87
P4ncake <jonathan.schmidt0@gmail.com>
2014-03-07 11:56:48 +0100

    gofmt

--- gsAPI.go
+++ gsAPI.go
@@ -1,20 +1,20 @@
 package gsAPI
 
-import(
-	"log"
-	"net/http"
+import (
 	"bytes"
 	"crypto/hmac"
 	"crypto/md5"
-	"encoding/json"
-	"io/ioutil"
-	"errors"
-	"strconv"
 	"encoding/hex"
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
 )
 
 type ErrorResponse struct {
-	Code int `json:"code"`
+	Code    int    `json:"code"`
 	Message string `json:"Message"`
 }
 
@@ -23,16 +23,15 @@ type HeaderResponse struct {
 }
 
 type ResultResponse struct {
-	Success	bool `json:"success"`
+	Success   bool   `json:"success"`
 	SessionId string `json:"sessionID"`
 }
 
 type JsonResponse struct {
-	Error []ErrorResponse `json:"errors"`
-	Header HeaderResponse `json:"header"`
-	Result ResultResponse `json:"result"`
+	Error  []ErrorResponse `json:"errors"`
+	Header HeaderResponse  `json:"header"`
+	Result ResultResponse  `json:"result"`
 }
-
 
 type Headers struct {
 	WsKey string `json:"wsKey"`
@@ -43,16 +42,16 @@ type Params struct {
 }
 
 type Payload struct {
-	Method string `json:"method"`
-	Parameters Params `json:"parameters"`
-	Header Headers `json:"header"`
+	Method     string  `json:"method"`
+	Parameters Params  `json:"parameters"`
+	Header     Headers `json:"header"`
 }
 
 var apiScheme = "http://"
 var apiHost = "api.grooveshark.com"
 var apiEndpoint = "/ws3.php"
 
-var WsKey ,WsSecret string
+var WsKey, WsSecret string
 
 func New(key string, secret string) {
 	WsKey = key
@@ -61,11 +60,11 @@ func New(key string, secret string) {
 
 func StartSession() (string, error) {
 	args := Params{}
-	result, err := makeCall("startSession",args, "sessionID", true, "")
+	result, err := makeCall("startSession", args, "sessionID", true, "")
 	if err != nil {
-		return "",err
+		return "", err
 	}
-	return result,nil
+	return result, nil
 }
 
 func createMessageSig(params string, secret string) string {
@@ -76,7 +75,7 @@ func ComputeHmacMD5(message string, secret string) string {
 	key := []byte(secret)
 	h := hmac.New(md5.New, key)
 	h.Write([]byte(message))
-    return hex.EncodeToString(h.Sum(nil))
+	return hex.EncodeToString(h.Sum(nil))
 
 }
 
@@ -85,12 +84,12 @@ func makeCall(method string, args Params, resultkey string, https bool, sessionI
 	if https {
 		apiScheme = "https://"
 	}
-	postData := `{"method":"`+method+`","header":{"wsKey":"`+WsKey+`"},"parameters":[]}`
+	postData := `{"method":"` + method + `","header":{"wsKey":"` + WsKey + `"},"parameters":[]}`
 	headers := Headers{WsKey: WsKey}
-	post := Payload{Method: method,Parameters: args,Header: headers }
+	post := Payload{Method: method, Parameters: args, Header: headers}
 	log.Print(post)
-	d,e := json.Marshal(post)
-	if e !=nil {
+	d, e := json.Marshal(post)
+	if e != nil {
 		log.Fatal(e.Error())
 	}
 	log.Print(string(d))
@@ -98,25 +97,25 @@ func makeCall(method string, args Params, resultkey string, https bool, sessionI
 
 	sig := createMessageSig(postData, WsSecret)
 	log.Print(sig)
-	requestUrl := apiScheme+apiHost+apiEndpoint+"?sig="+sig
+	requestUrl := apiScheme + apiHost + apiEndpoint + "?sig=" + sig
 	log.Print(requestUrl)
-	resp, err := http.Post(requestUrl,"text/json charset=UTF-8",content)
+	resp, err := http.Post(requestUrl, "text/json charset=UTF-8", content)
 	if err != nil {
-		log.Fatal("Err POST: "+ err.Error())
+		log.Fatal("Err POST: " + err.Error())
 	}
 	body, readerr := ioutil.ReadAll(resp.Body)
 	if readerr != nil {
-		log.Fatal("Err READALL: "+ err.Error())
+		log.Fatal("Err READALL: " + err.Error())
 	}
 	log.Print(string(body))
 	var jsonresponse JsonResponse
-	err = json.Unmarshal(body,&jsonresponse)
+	err = json.Unmarshal(body, &jsonresponse)
 	log.Print(jsonresponse)
 	if err != nil {
-		log.Fatal("Err : "+ err.Error())
+		log.Fatal("Err : " + err.Error())
 	}
 	if len(jsonresponse.Error) != 0 {
-		return "", errors.New(strconv.Itoa(jsonresponse.Error[0].Code)+" - "+jsonresponse.Error[0].Message)
+		return "", errors.New(strconv.Itoa(jsonresponse.Error[0].Code) + " - " + jsonresponse.Error[0].Message)
 	}
 
 	return "yes", nil
