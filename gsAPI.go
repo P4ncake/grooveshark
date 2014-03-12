171cf53f72adfa30a415d595a3c7788df0cf377a
P4ncake <jonathan.schmidt0@gmail.com>
2014-03-12 00:11:45 +0000

    Auth and ping added, Json handler updated

--- gsAPI.go
+++ gsAPI.go
@@ -9,7 +9,7 @@ import(
 	"encoding/json"
 	"io/ioutil"
 	"errors"
-	"strconv"
+	_ "strconv"
 	"encoding/hex"
 )
 
@@ -30,21 +30,18 @@ type ResultResponse struct {
 type JsonResponse struct {
 	Error []ErrorResponse `json:"errors"`
 	Header HeaderResponse `json:"header"`
-	Result ResultResponse `json:"result"`
+	Result map[string]interface{} `json:"result"`
 }
 
 
 type Headers struct {
 	WsKey string `json:"wsKey"`
-}
-
-type Params struct {
-	Name string `json:"name"`
+	SessionID string `json:"sessionID"`
 }
 
 type Payload struct {
 	Method string `json:"method"`
-	Parameters Params `json:"parameters"`
+	Parameters interface{} `json:"parameters"`
 	Header Headers `json:"header"`
 }
 
@@ -53,26 +50,64 @@ var apiHost = "api.grooveshark.com"
 var apiEndpoint = "/ws3.php"
 
 var WsKey ,WsSecret string
+var sessionID string
 
 func New(key string, secret string) {
 	WsKey = key
 	WsSecret = secret
 }
 
+func PingService() (string, error){
+	args := map[string]interface{}{}
+	result, err := makeCall("pingService",args,"",true,"")
+	if err != nil {
+		return "", err
+	}
+	log.Fatal(result)
+	return "", nil
+}
+
 func StartSession() (string, error) {
-	args := Params{}
+
+	args := map[string]interface{}{}
 	result, err := makeCall("startSession",args, "sessionID", true, "")
 	if err != nil {
 		return "",err
 	}
-	return result,nil
+	sessionID = result.(map[string]interface{})["sessionID"].(string)
+	return sessionID, nil
+}
+
+func Authenticate(username string, password string) (interface{}, error) {
+	if username == "" || password == "" {
+		return nil, errors.New("Empty username or password")
+	}
+
+	h := md5.New()
+	h.Write([]byte(password))
+	hpass := hex.EncodeToString(h.Sum(nil))
+
+	args := map[string]interface{}{
+		"login":username,
+		"password" : hpass,
+	}
+
+	result, err := makeCall("authenticate",args,"",true,sessionID)
+	if err != nil {
+		return nil, err
+	}
+
+	if !result.(map[string]interface{})["success"].(bool) {
+		return nil, errors.New("Login failed. invalid username/password")
+	}
+	return result, nil
 }
 
 func createMessageSig(params string, secret string) string {
-	return ComputeHmacMD5(params, secret)
+	return computeHmacMD5(params, secret)
 }
 
-func ComputeHmacMD5(message string, secret string) string {
+func computeHmacMD5(message string, secret string) string {
 	key := []byte(secret)
 	h := hmac.New(md5.New, key)
 	h.Write([]byte(message))
@@ -80,26 +115,22 @@ func ComputeHmacMD5(message string, secret string) string {
 
 }
 
-func makeCall(method string, args Params, resultkey string, https bool, sessionID string) (string, error) {
+func makeCall(method string, args interface{}, resultkey string, https bool, sessionID string) (interface{}, error) {
 
 	if https {
 		apiScheme = "https://"
 	}
-	postData := `{"method":"`+method+`","header":{"wsKey":"`+WsKey+`"},"parameters":[]}`
-	headers := Headers{WsKey: WsKey}
+	headers := Headers{WsKey: WsKey, SessionID: sessionID}
 	post := Payload{Method: method,Parameters: args,Header: headers }
-	log.Print(post)
 	d,e := json.Marshal(post)
 	if e !=nil {
 		log.Fatal(e.Error())
 	}
-	log.Print(string(d))
+//	log.Print(string(d))
 	content := bytes.NewBuffer([]byte(d))
 
-	sig := createMessageSig(postData, WsSecret)
-	log.Print(sig)
+	sig := createMessageSig(string(d), WsSecret)
 	requestUrl := apiScheme+apiHost+apiEndpoint+"?sig="+sig
-	log.Print(requestUrl)
 	resp, err := http.Post(requestUrl,"text/json charset=UTF-8",content)
 	if err != nil {
 		log.Fatal("Err POST: "+ err.Error())
@@ -109,20 +140,17 @@ func makeCall(method string, args Params, resultkey string, https bool, sessionI
 		log.Fatal("Err READALL: "+ err.Error())
 	}
 	log.Print(string(body))
-	var jsonresponse JsonResponse
+	var jsonresponse map[string]interface{}
 	err = json.Unmarshal(body,&jsonresponse)
 	log.Print(jsonresponse)
 	if err != nil {
 		log.Fatal("Err : "+ err.Error())
 	}
-	if len(jsonresponse.Error) != 0 {
-		return "", errors.New(strconv.Itoa(jsonresponse.Error[0].Code)+" - "+jsonresponse.Error[0].Message)
+
+/*	if len(jsonresponse["Error"]) != 0 {
+		return nil, errors.New(strconv.Itoa(jsonresponse.Error[0].Code)+" - "+jsonresponse.Error[0].Message)
 	}
-
-	return "yes", nil
+*/
+	return jsonresponse["result"], nil
 }
 
-func handleError(v interface{}) {
-	log.Fatal(v)
-
-}
